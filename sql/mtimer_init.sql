175d7a03c5baf801cc3896fdfa9eb889aa338cbc
赵权 <zhaoquan@mobike.com>
2018-08-08 16:03:03 +0800

    add dao

---
+++ sql/mtimer_init.sql
@@ -0,0 +1,17 @@
+CREATE TABLE mtimer_tasks (
+	`id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键',
+	`group_id` int(10) UNSIGNED NOT NULL COMMENT '业务组ID',
+	`group_name` varchar(20) COMMENT '业务组名称',
+	`biz_id` int(10) UNSIGNED NOT NULL COMMENT '业务ID',
+	`biz_name` varchar(20) COMMENT '业务名称',
+	`cron_time` varchar(20) NOT NULL COMMENT 'cron表达式，用来执行定时任务',
+	`status` tinyint(2) NOT NULL COMMENT '任务状态：0:new;1:已加载;2:中断;3:执行成功;4删除:',
+	`ip` varchar(16) COMMENT '创建服务器地址',
+	`param` varchar(256) COMMENT '回调参数，json格式',
+	`ins_num` int(10) UNSIGNED COMMENT '实例分区号：用户多实例部署',
+	`excution_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '任务执行时间，对应于cron_time',
+	`create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+	`update_time` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+	PRIMARY KEY (`id`),
+	INDEX `uniq_mtimer_group_biz` (`group_id`, `biz_id`)
+) ENGINE=`InnoDB` COMMENT='定时服务器调度任务表';
---
+++ tasks/MtimerTask.go
@@ -0,0 +1,139 @@
+package tasks
+
+import (
+	"time"
+	"mtimer/db/mysql"
+	"database/sql"
+	"log"
+	"fmt"
+)
+
+const (
+	NewBee = iota
+	Loaded
+	Interupt
+	Success
+	Del
+)
+
+const (
+	table_name = "mtimer_tasks"
+	table_fields_insert = "group_id, group_name, biz_id, biz_name, cron_time, status, ip, param, ins_num, excution_time, create_time, update_time"
+	table_fields_all = "id, group_id, group_name, biz_id, biz_name, cron_time, status, ip, param, ins_num, excution_time, create_time, update_time"
+)
+
+
+type MtimerTask struct {
+	Id int64
+	GroupId int
+	GroupName string
+	BizId int
+	BizName string
+	CronTime string
+	Status int
+	Ip string
+	Param string
+	InsNum int
+	ExcutionTime time.Time
+	CreateTime time.Time
+	UpdateTime time.Time
+}
+
+func (task *MtimerTask) CreateNewOneTask() (int64, error) {
+	db := mysql.GetDB()
+
+	//log.Println(task.GroupId, task.GroupName, task.BizId, task.BizName, task.CronTime, task.Status, task.Ip, task.Param, task.InsNum, task.ExcutionTime)
+
+	stmt, err := db.Prepare(fmt.Sprintf("INSERT INTO %s(%s) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", table_name, table_fields_insert))
+	//stmt, err := db.Prepare(fmt.Sprintf("INSERT INTO %s(%s) VALUES(?, ?, ?, ?)", table_name, "group_id, biz_id, cron_time, status"))
+	if err != nil {
+		return 0, err
+	}
+
+	defer stmt.Close()
+
+	var result sql.Result
+	result, err = stmt.Exec(task.GroupId, task.GroupName, task.BizId, task.BizName, task.CronTime, task.Status, task.Ip, task.Param, task.InsNum, task.ExcutionTime, task.CreateTime, task.UpdateTime)
+	//result, err = stmt.Exec(1, 1, " * * * * * ", NewBee)
+	if err != nil {
+		return 0, err
+	}
+
+	var rows int64
+	rows, err = result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+	if rows > 0 {
+		return rows, nil
+	}
+	return 0, nil
+}
+
+func (task *MtimerTask) StopOneTask() (bool, error) {
+	if task.Status == NewBee || task.Status == Loaded {
+		return updateTaskStatus(Interupt, task.Id)
+	}
+	return false, nil
+}
+
+func (task *MtimerTask) DelOneTask() (bool, error) {
+	return updateTaskStatus(Del, task.Id)
+}
+
+func (task *MtimerTask) TaskSuccess() (bool, error) {
+	return updateTaskStatus(Success, task.Id)
+}
+
+
+func GetTaskList(partition int) ([]MtimerTask, error) {
+	db := mysql.GetDB()
+
+	rows, err := db.Query("SELECT " + table_fields_all + " FROM " + table_name + " WHERE parition=? AND status in (" + fmt.Sprintf("%d, %d", NewBee, Loaded) + ") order by excution_time desc limit 500", partition)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	lists := make([]MtimerTask, 500)
+
+	for rows.Next() {
+		task := MtimerTask{}
+		err = rows.Scan(&task.Id, &task.GroupId, &task.GroupName, &task.BizId, &task.BizName, &task.CronTime, &task.Status, &task.Ip, &task.Param, &task.ExcutionTime, &task.CreateTime, &task.UpdateTime)
+		if err != nil {
+			continue
+		}
+		_ = append(lists, task)
+	}
+	rows.Close()
+
+	if err = rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return lists, nil
+}
+
+func updateTaskStatus(status int, id int64) (bool, error) {
+	db := mysql.GetDB()
+	stmt, err := db.Prepare("UPDATE " + table_name + " SET status=? WHERE id=?")
+	if err != nil {
+		return false, err
+	}
+
+	defer stmt.Close()
+
+	var result sql.Result
+	var rows int64
+	result, err = stmt.Exec(status, id)
+	rows, err = result.LastInsertId()
+	if err !=nil {
+		return false, err
+	}
+	if rows > 0 {
+		return true, nil
+	}
+
+	return false, nil
+}
